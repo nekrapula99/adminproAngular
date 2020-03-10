ba380e6a2c3d49217c102bcf67e536b2b18d5016
nosorio <nosorio@bancolombia.com.co>
2020-03-10 10:10:11 -0500

    Modificación en Modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+const pageRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            {path: 'dashboard', component: DashboardComponent},
+            {path: 'progress', component: ProgressComponent},
+            {path: 'graficas1', component: Graficas1Component},
+            {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // Me redirecciona al dash cualquier otra
+
+        ]
+    },
+];
+
+export const PAGES_ROUTES = RouterModule.forChild(pageRoutes);
